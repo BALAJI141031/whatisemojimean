dd9f0e8030d4fcae8bc46cf91d6d815202a3dc17
BALAJI141031 <85923874+BALAJI141031@users.noreply.github.com>
2021-08-22 21:23:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import "./styles.css";
+import { useState } from "react";
+
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "🧗": "Person Climbing",
+  "🏇": "Horse Riding",
+  "🚣": "RowBoat"
+};
+
+var emojiList = Object.keys(emojiDictionary);
+console.log(emojiList);
+
+export default function App() {
+  var [state, updateState] = useState("you will know the meaning of emoji");
+  var [inputEmoji, updateInputEmoji] = useState("");
+
+  function updateStateWithEmoji(event) {
+    var emoji = event.target.value;
+
+    updateInputEmoji(emoji);
+    var emojiMean = emojiDictionary[emoji];
+    //console.log(event.target.value)
+    if (emojiMean === undefined) {
+      updateState("That emoji is not in our database");
+    } else {
+      updateState(emojiMean);
+    }
+  }
+
+  function findEmoji(emoji) {
+    console.log(emoji);
+
+    var emojiMeanBytClick = emojiDictionary[emoji];
+    updateState(emojiMeanBytClick);
+  }
+
+  return (
+    <div className="App">
+      <h1> Lets Know about Emoji</h1>
+      <input
+        placeholder="give your emoji here"
+        className="input-el"
+        onChange={updateStateWithEmoji}
+      />
+      <div className="input-emoji">{inputEmoji}</div>
+      <div className="emojiMean">{state}</div>
+      {emojiList.map(function (emoji) {
+        return (
+          <li
+            className="emoji"
+            key={emoji}
+            onClick={function () {
+              findEmoji(emoji);
+            }}
+          >
+            {emoji}
+          </li>
+        );
+      })}
+    </div>
+  );
+}
